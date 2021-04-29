82921a45b1aadd047431aeb67f7c4b7cd1c1b83c
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-04-28 22:00:52 -0300

    Convert audio files for ogg extension

--- Characters/Rocket.gd
+++ Characters/Rocket.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 var speed = 30
 var velocity = Vector2.ZERO
 
-func _physics_process(delta):
+func _physics_process(_delta):
     if Input.is_action_pressed("right"):
         velocity.x = speed
     elif Input.is_action_pressed("left"):
