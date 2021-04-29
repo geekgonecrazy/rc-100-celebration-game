435b6b6ecf4d724a4c0853f94f9e2eb674c67843
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-04-29 02:10:48 -0300

    add bg and fix rocket motion with delta

--- Characters/Rocket.gd
+++ Characters/Rocket.gd
@@ -1,14 +1,14 @@
 extends KinematicBody2D
 
-var speed = 400
+var speed = 300
 var velocity = Vector2.ZERO
 onready var vp_size = get_viewport().size
 
-func _physics_process(_delta):
+func _physics_process(delta):
     if Input.is_action_pressed("right"):
-        velocity.x = speed
+        velocity.x = speed * delta * 100
     elif Input.is_action_pressed("left"):
-        velocity.x = -speed
+        velocity.x = -speed * delta * 100
     else:
         velocity.x = lerp(velocity.x, 0, 0.2)
 
