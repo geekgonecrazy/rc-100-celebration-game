f3a52a6dac44f3a2fb7d6a3cf3c6e919b0d5d6a2
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-04-29 01:53:36 -0300

    rocket script adjustments

--- Characters/Rocket.gd
+++ Characters/Rocket.gd
@@ -2,18 +2,21 @@ extends KinematicBody2D
 
 var Bullet = preload("res://Characters/Bullet.tscn")
 
-var speed = 50
+var speed = 400
 var velocity = Vector2.ZERO
+onready var vp_size = get_viewport().size
 
 func _physics_process(_delta):
-	if Input.is_action_pressed("right"):
-		velocity.x = speed
-	elif Input.is_action_pressed("left"):
-		velocity.x = -speed
-	else:
-		velocity.x = 0
-		
-	move_and_slide(velocity)
+    if Input.is_action_pressed("right"):
+        velocity.x = speed
+    elif Input.is_action_pressed("left"):
+        velocity.x = -speed
+    else:
+        velocity.x = lerp(velocity.x, 0, 0.2)
+
+    position.x = clamp(position.x, 30, vp_size.x - 30)
+
+    move_and_slide(velocity)
 	
 	if Input.is_action_just_pressed("shot"):
 		shoot()
